e716af1946b3a3a9e4a99497977675e7ae75a4b3
Kako Botasso <kakodev@gmail.com>
2017-02-19 18:02:57 -0300

    Personaliza Toolbar

--- app/src/main/java/br/com/kakobotasso/lodjinha/MainActivity.java
+++ app/src/main/java/br/com/kakobotasso/lodjinha/MainActivity.java
@@ -1,9 +1,6 @@
 package br.com.kakobotasso.lodjinha;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,7 +8,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -21,27 +17,17 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setItemIconTintList(null);
-        navigationView.setNavigationItemSelectedListener(this);
-        View headerView =  navigationView.getHeaderView(0);
-
-        TextView test = (TextView) headerView.findViewById(R.id.nav_titulo_menu);
-        Fonts.aplicaEstilo(this, test);
+        preparaToolbar();
+        preparaNavigationDrawer();
+        preparaNaviationView();
     }
 
     @Override
@@ -60,5 +46,37 @@ public class MainActivity extends AppCompatActivity
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void preparaToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        personalizaToolbar();
+        setSupportActionBar(toolbar);
+    }
+
+    private void personalizaToolbar(){
+        TextView titulo = (TextView) toolbar.findViewById(R.id.toolbar_titulo);
+        Fonts.aplicaEstilo(this, titulo);
+    }
+
+    private void preparaNavigationDrawer(){
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void preparaNaviationView(){
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+        personalizaNavigationView();
+    }
+
+    private void personalizaNavigationView(){
+        View headerView =  navigationView.getHeaderView(0);
+        TextView test = (TextView) headerView.findViewById(R.id.nav_titulo_menu);
+        Fonts.aplicaEstilo(this, test);
     }
 }
