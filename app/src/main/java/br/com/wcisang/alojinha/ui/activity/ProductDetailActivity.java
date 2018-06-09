e12c69fcf9c626a8cc4b87f7c72749b5449d52e5
WCisang <william.zischang.cisang@gmail.com>
2018-06-08 21:54:39 -0300

    better layout on lists

--- app/src/main/java/br/com/wcisang/alojinha/ui/activity/ProductDetailActivity.java
+++ app/src/main/java/br/com/wcisang/alojinha/ui/activity/ProductDetailActivity.java
@@ -4,22 +4,30 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import br.com.wcisang.alojinha.R;
 import br.com.wcisang.alojinha.databinding.ActivityProductDetailBinding;
 import br.com.wcisang.alojinha.model.Product;
 import br.com.wcisang.alojinha.viewmodel.ProductDetailViewModel;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
     public static final String PRODUCT = "product";
     private ProductDetailViewModel viewModel;
     private Product product;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityProductDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         viewModel = ViewModelProviders.of(this).get(ProductDetailViewModel.class);
         product = (Product) getIntent().getSerializableExtra(PRODUCT);
         binding.setProduct(product);
