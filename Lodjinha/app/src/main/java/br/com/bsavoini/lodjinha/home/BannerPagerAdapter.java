b6f8e7ccb7aa0cb98b8d9f0b9dc15386e3e9caf4
bsavoini <bruno.savoini@gmail.com>
2019-01-07 00:36:42 -0200

    show banners

---
+++ Lodjinha/app/src/main/java/br/com/bsavoini/lodjinha/home/BannerPagerAdapter.java
@@ -0,0 +1,52 @@
+package br.com.bsavoini.lodjinha.home;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import br.com.bsavoini.lodjinha.R;
+import br.com.bsavoini.lodjinha.api.model.BannerModel;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import java.util.List;
+
+public class BannerPagerAdapter extends PagerAdapter {
+    private List<BannerModel> banners;
+    private RequestManager glideRequest;
+
+    public BannerPagerAdapter(List<BannerModel> banners) {
+        this.banners = banners;
+    }
+
+    @Override
+    public int getCount() {
+        return banners.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return obj == view;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(container.getContext());
+        View view = inflater.inflate(R.layout.item_banner, container, false);
+        container.addView(view);
+
+        ImageView imgBanner = view.findViewById(R.id.img_banner);
+
+        glideRequest = Glide.with(container.getContext());
+        glideRequest.load(banners.get(position).getImageURL()).into(imgBanner);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+}
+
--- Lodjinha/app/src/main/java/br/com/bsavoini/lodjinha/home/HomeFragment.java
+++ Lodjinha/app/src/main/java/br/com/bsavoini/lodjinha/home/HomeFragment.java
@@ -1,18 +1,45 @@
 package br.com.bsavoini.lodjinha.home;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import br.com.bsavoini.lodjinha.R;
+import br.com.bsavoini.lodjinha.api.RetrofitInstance;
+import br.com.bsavoini.lodjinha.api.model.BannersResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
-    @Nullable
+    private ViewPager bannersViewPager;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        bannersViewPager = view.findViewById(R.id.view_pager_banners);
+        requestBanners();
+    }
+//TODO call request from interactor
+    private void requestBanners() {
+        Call<BannersResponse> bannersResponseCall = RetrofitInstance.getLodjinhaService().requestBanner();
+        bannersResponseCall.enqueue(new Callback<BannersResponse>() {
+            @Override
+            public void onResponse(Call<BannersResponse> call, Response<BannersResponse> response) {
+
+                bannersViewPager.setAdapter(new BannerPagerAdapter(response.body().getBannersArr()));
+
+            }
+
+            @Override
+            public void onFailure(Call<BannersResponse> call, Throwable t) {
+            }
+        });
     }
 }
