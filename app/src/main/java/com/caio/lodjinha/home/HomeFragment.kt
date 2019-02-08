b969c41c32617c2baceb7ea082d66b49b588c1b1
caio machado <ccouto@imonitore.com.br>
2019-02-08 14:57:35 -0200

    create banner Home

--- app/src/main/java/com/caio/lodjinha/home/HomeFragment.kt
+++ app/src/main/java/com/caio/lodjinha/home/HomeFragment.kt
@@ -4,9 +4,16 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.caio.lodjinha.R
 import com.caio.lodjinha.base.BaseFragment
+import com.caio.lodjinha.home.adapter.BannerPagerAdapter
 import com.caio.lodjinha.viewmodel.BannerViewModel
+import com.caio.lodjinha.viewmodel.CategoryViewModel
 import com.caio.lodjinha.viewmodel.ProductViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : BaseFragment() {
@@ -14,6 +21,8 @@ class HomeFragment : BaseFragment() {
     private lateinit var productViewModel: ProductViewModel
 
     private lateinit var bannerViewModel: BannerViewModel
+
+    private lateinit var categoryViewModel: CategoryViewModel
 
     companion object {
 
@@ -29,17 +38,28 @@ class HomeFragment : BaseFragment() {
     private fun initViewModel() {
         productViewModel = ViewModelProviders.of(this).get(ProductViewModel::class.java)
         bannerViewModel = ViewModelProviders.of(this).get(BannerViewModel::class.java)
+        categoryViewModel = ViewModelProviders.of(this).get(CategoryViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
 
+        createBanner()
+    }
 
+    private fun createBanner() {
         bannerViewModel.getBanner()?.observe(this, Observer {
             if (it!!.isSuccess()) {
-                Log.d("BANNER", it.data.toString()+" mídias encontradas")
+                viewPagerBanner.adapter = BannerPagerAdapter(context!!, it.data!!.data)
+                tabLayoutBanner.setupWithViewPager(viewPagerBanner, true)
             }
         })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
---
+++ app/src/main/java/com/caio/lodjinha/home/adapter/BannerPagerAdapter.kt
@@ -0,0 +1,41 @@
+package com.caio.lodjinha.home.adapter
+
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.caio.lodjinha.R
+import com.caio.lodjinha.extensions.loadUrl
+import com.caio.lodjinha.repository.entity.Banner
+import kotlinx.android.synthetic.main.frame_layout_banner.view.*
+
+class BannerPagerAdapter(var mContext: Context, internal var banners: List<Banner>?) : PagerAdapter() {
+
+    var mLayoutInflater: LayoutInflater = mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return banners!!.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as FrameLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val itemView = mLayoutInflater.inflate(R.layout.frame_layout_banner, container, false)
+
+        val banner = banners!![position]
+
+        itemView.imageViewBanner.loadUrl(banner.urlImagem,itemView.progress)
+
+        container.addView(itemView)
+
+        return itemView
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as FrameLayout)
+    }
+}
