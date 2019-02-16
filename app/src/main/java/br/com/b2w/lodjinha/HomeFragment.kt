b437653857e159ad30ae170cf0a853d660073c34
Lucas Candalo <candalolucas@gmail.com>
2019-02-16 17:28:51 -0200

    Add home fragment
    Signed-off-by: Lucas Candalo <candalolucas@gmail.com>

---
+++ app/src/main/java/br/com/b2w/lodjinha/HomeFragment.kt
@@ -0,0 +1,16 @@
+package br.com.b2w.lodjinha
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+
+}
---
+++ app/src/main/java/br/com/b2w/lodjinha/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.b2w.lodjinha
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
