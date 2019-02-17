4bb43afbae92caee8f6b512edc2ee8a143678e4e
Lucas Candalo <candalolucas@gmail.com>
2019-02-17 15:02:38 -0300

    Fix product new price position
    Signed-off-by: Lucas Candalo <candalolucas@gmail.com>

--- app/src/main/java/br/com/b2w/lodjinha/features/product/presentation/ProductsFragment.kt
+++ app/src/main/java/br/com/b2w/lodjinha/features/product/presentation/ProductsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.b2w.lodjinha.BaseFragment
@@ -42,6 +43,7 @@ class ProductsFragment : BaseFragment() {
 
     private fun setupRecyclerView() {
         with(productsRecyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             adapter = this@ProductsFragment.adapter
         }
