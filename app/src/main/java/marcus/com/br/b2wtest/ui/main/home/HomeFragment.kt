89613069fb1cf36822292ae6612218467feb852e
Marcus Souza <marcus.souza@concrete.com.br>
2019-02-23 17:24:11 -0300

    Reorganize layout

--- app/src/main/java/marcus/com/br/b2wtest/ui/main/home/HomeFragment.kt
+++ app/src/main/java/marcus/com/br/b2wtest/ui/main/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.support.v7.widget.PagerSnapHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.include_banner.*
 import marcus.com.br.b2wtest.R
 import marcus.com.br.b2wtest.helper.snap.OnSnapPositionChangeListener
 import marcus.com.br.b2wtest.helper.snap.attachSnapHelperWithListener
