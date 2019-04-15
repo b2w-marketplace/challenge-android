43b8621c5ea21437e06e280d5a50017b1cfdef1e
Dafle dos Santos Cardoso - DCR <dafle.cardoso@iteris.com.br>
2019-04-15 01:42:49 -0300

    Compatibilidade com api 15

--- app/src/androidTest/java/br/com/dafle/alodjinha/LodjinhaApplicationTest.kt
+++ app/src/androidTest/java/br/com/dafle/alodjinha/LodjinhaApplicationTest.kt
@@ -1,10 +1,14 @@
 package br.com.dafle.alodjinha
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import androidx.multidex.MultiDex
 import br.com.dafle.alodjinha.koin.ModulesTest
 import org.koin.android.ext.android.startKoin
 
-class LodjinhaApplicationTest: Application() {
+class LodjinhaApplicationTest: Application(), Application.ActivityLifecycleCallbacks {
 
     override fun onCreate() {
         super.onCreate()
@@ -14,5 +18,38 @@ class LodjinhaApplicationTest: Application() {
                 ModulesTest.viewModelModule
             )
         )
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onActivityPaused(activity: Activity?) {
+
+    }
+
+    override fun onActivityResumed(activity: Activity?) {
+
+    }
+
+    override fun onActivityStarted(activity: Activity?) {
+
+    }
+
+    override fun onActivityDestroyed(activity: Activity?) {
+
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+
+    }
+
+    override fun onActivityStopped(activity: Activity?) {
+
+    }
+
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+
     }
 }
--- app/src/main/java/br/com/dafle/alodjinha/LodjinhaApplication.kt
+++ app/src/main/java/br/com/dafle/alodjinha/LodjinhaApplication.kt
@@ -1,10 +1,16 @@
 package br.com.dafle.alodjinha
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.os.Bundle
 import br.com.dafle.alodjinha.koin.Modules
 import org.koin.android.ext.android.startKoin
+import androidx.multidex.MultiDex
 
-open class LodjinhaApplication : Application() {
+
+
+open class LodjinhaApplication : Application(), Application.ActivityLifecycleCallbacks {
 
     override fun onCreate() {
         super.onCreate()
@@ -14,5 +20,38 @@ open class LodjinhaApplication : Application() {
                 Modules.viewModelModule
             )
         )
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onActivityPaused(activity: Activity?) {
+
+    }
+
+    override fun onActivityResumed(activity: Activity?) {
+
+    }
+
+    override fun onActivityStarted(activity: Activity?) {
+
+    }
+
+    override fun onActivityDestroyed(activity: Activity?) {
+
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+
+    }
+
+    override fun onActivityStopped(activity: Activity?) {
+
+    }
+
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+
     }
 }
