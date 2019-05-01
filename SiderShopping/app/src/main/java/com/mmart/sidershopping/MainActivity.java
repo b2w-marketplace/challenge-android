061d21ba831cdf5f68b84deb6b8ee97d4d368ab2
Matheus Martins de Sales <m.martins.sales@hotmail.com>
2019-04-30 21:28:49 -0300

    Slider
    Criado o slider com imagens estaticas.

--- SiderShopping/app/src/main/java/com/mmart/sidershopping/MainActivity.java
+++ SiderShopping/app/src/main/java/com/mmart/sidershopping/MainActivity.java
@@ -1,13 +1,35 @@
 package com.mmart.sidershopping;
 
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] imageUrls = new String[]{
+            "https://cdn.pixabay.com/photo/2016/11/11/23/34/cat-1817970_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/12/21/12/26/glowworm-3031704_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/12/24/09/09/road-3036620_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_960_720.jpg",
+            "https://cdn.pixabay.com/photo/2017/10/10/15/28/butterfly-2837589_960_720.jpg"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this, imageUrls);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        tabLayout.setupWithViewPager(viewPager, true);
+
+        viewPager.setAdapter(adapter);
+
+
     }
 }
