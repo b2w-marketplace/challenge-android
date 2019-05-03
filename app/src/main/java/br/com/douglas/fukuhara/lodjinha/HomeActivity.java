a3d642dd8279f4b5d13cd846da6aac41be26a017
Douglas Fukuhara <dofukuhara@gmail.com>
2019-05-03 01:04:15 -0300

    Initial implementation of Navigation Drawer

--- app/src/main/java/br/com/douglas/fukuhara/lodjinha/HomeActivity.java
+++ app/src/main/java/br/com/douglas/fukuhara/lodjinha/HomeActivity.java
@@ -1,13 +1,59 @@
 package br.com.douglas.fukuhara.lodjinha;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        initDrawerComponents();
+    }
+
+    private void initDrawerComponents() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.home_drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.drawer_open_desc, R.string.drawer_close_desc);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.home_drawer_nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id) {
+            case R.id.home_drawer_menu_home:
+                // TODO - Implement launcher of Home from Navigation Drawer
+                break;
+            case R.id.home_drawer_menu_about:
+                // TODO - Implement launcher of About Screen from Navigation Drawer
+                break;
+        }
+
+//        menuItem.setChecked(false);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
